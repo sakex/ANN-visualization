796cf77f5822cb66d501eb4b7712c397a1fcc9f2
sakex <alexandre@senges.ch>
2020-07-15 16:58:26 +0200

    Remove curve

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,14 @@ import Head from 'next/head';
 import Graph from '../components/Graph';
 import { PhenotypeParser } from '../src/PhenotypeParser';
 import nodes from '../data/nodes.json';
-import Strategy from '../components/Strategy';
-import curve from '../data/curve.json';
 
 const Home = () => (
     <div>
         <Head>
             <title>Home</title>
         </Head>
-        <Graph parser={new PhenotypeParser()} data={nodes} width={400000} height={800} />
+        <Graph parser={new PhenotypeParser()} data={nodes.phenotypes} width={400000} height={800} />
 
-        <Strategy data={curve} width={1800} height={800} />
     </div>
 );
 
